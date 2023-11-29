5e0a273d61169652387855ea94410827c551cf72
Kristína HREBEŇÁROVÁ <kristina.hrebenarova@asseco-ce.com>
2023-11-29 16:55:17 +0100

    feat: add npm packages, set up navigation bar

--- ClientApp/src/app/components/navigation/navigation.component.ts
+++ ClientApp/src/app/components/navigation/navigation.component.ts
@@ -4,7 +4,7 @@ import { UserService } from 'src/app/services/user/user.service';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
 
@@ -15,6 +15,16 @@ export class NavigationComponent implements OnInit {
       this.userService.user = x.name
       console.log(x);
     });
+  }
+
+  isExpanded = false;
+
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
   }
 
 }
