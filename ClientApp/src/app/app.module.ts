2087d6bddac51929ec2adc59e52d397ea6ae77d8
Kristína HREBEŇÁROVÁ <kristina.hrebenarova@asseco-ce.com>
2023-11-29 18:26:55 +0100

    feat: changes to theme, implement login/registration component

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,36 +1,56 @@
+// angular-specific
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
+// app-modules
+import { AppRoutingModule } from './app-routing.module';
+
+// app-components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
-import { FormsModule } from '@angular/forms';
-import { UserService } from './services/user/user.service';
-import { HttpClientModule } from '@angular/common/http';
 import { RoomComponent } from './pages/room/room.component';
 
+// app-services
+import { UserService } from './services/user/user.service';
+
+// primeng-related
+import { MessagesModule } from 'primeng/messages';
+import { MessageService, SharedModule } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+
+
 @NgModule({
-  declarations: [		
+  declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
     NavigationComponent,
     WelcomeComponent,
-    RoomComponent
-   ],
+    RoomComponent,
+  ],
   imports: [
     RouterModule.forRoot(AppRoutingModule),
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    SharedModule,
+    MessagesModule,
+    ToastModule,
+    InputTextModule,
+    CardModule,
+    ButtonModule
   ],
-  providers: [
-    UserService
-  ],
-  bootstrap: [AppComponent]
+  providers: [UserService, MessageService, DatePipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- ClientApp/src/app/pages/login/login.component.ts
+++ ClientApp/src/app/pages/login/login.component.ts
@@ -4,21 +4,35 @@ import { UserService } from 'src/app/services/user/user.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   user = {
-    username: "",
-    password: "",
-  }
-  constructor(private userService: UserService) { }
+    username: '',
+    password: '',
+    email: '',
+  };
 
-  ngOnInit() {
-  }
+  submitted: boolean = false;
+  registration: boolean = false;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {}
 
   public submitLogin() {
-    this.userService.login(this.user).subscribe(x => this.userService.user = x.username);
+    this.userService
+      .login(this.user)
+      .subscribe((x) => (this.userService.user = x.username));
   }
 
+  public submitRegistration() {
+    this.userService
+      .register(this.user)
+      .subscribe((x) => (this.userService.user = x.username));
+  }
+
+  public switchToRegistration() {
+    this.registration = true;
+  }
 }
